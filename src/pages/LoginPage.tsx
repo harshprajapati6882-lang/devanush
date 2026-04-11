ec77b4064a913470f07fc81823b9c4a857854caa
harshprajapati6882-lang <harshprajapati6882@gmail.com>
2026-04-12 00:10:31 +0530

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -27,7 +27,6 @@ export default function LoginPage() {
       }
 
       // 🔥 THIS IS STEP 3.8 (you were confused here)
-      localStorage.clear(); // 🔥 clear old user data
       localStorage.setItem("token", data.token);
       localStorage.setItem("user", JSON.stringify(data.user));
 
